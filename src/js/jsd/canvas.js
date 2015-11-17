70fe89de8165fb88fced4725337cf06fac6e4c9f
Alex Milanov <alex@i4web.biz>
2015-11-17 19:53:36 +0200

    initial functionality - fixes #2

--- src/js/jsd/canvas.js
+++ src/js/jsd/canvas.js
@@ -5,6 +5,8 @@ if(typeof jsd === "undefined"){ jsd = {}; }
 jsd.Canvas = function(dom, context){
 	gui.Element.call(this, dom, context);
 
+	this.context = context;
+
 	this.canvas = $(dom)[0];
 	this.ctx = this.canvas.getContext("2d");
 
@@ -18,6 +20,9 @@ jsd.Canvas.prototype.constructor = jsd.Canvas;
 
 jsd.Canvas.prototype.init = function(){
 	gui.Element.prototype.init.call(this);
+
+	$(this.canvas).width(this.context.dimentions.width || 640);
+	$(this.canvas).width(this.context.dimentions.height || 480);
 
 	var painting = false;
 	var self = this;
@@ -38,7 +43,11 @@ jsd.Canvas.prototype.init = function(){
 
 	$(this._dom).mouseup(function(e){
 		painting = false;
-		self.paths.push(self.currentPath);
+		self.paths.push({
+			points: self.currentPath,
+			color: self.context.colors.fg || "#000000",
+			brush: _.clone(self.context.brush)
+		});
 		self.currentPath = [];
 	});
 
@@ -55,21 +64,25 @@ jsd.Canvas.prototype.refresh = function(){
 
 	var self = this;
 
+	$(this.canvas).width(this.context.dimentions.width || 640);
+	$(this.canvas).width(this.context.dimentions.height || 480);
 
 	this.canvas.width = $(this.canvas).width();
 	this.canvas.height = $(this.canvas).height();
 
 	this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height); // Clears the canvas
 	
-	this.ctx.strokeStyle = "#df4b26";
 	this.ctx.lineJoin = "round";
 
 	function drawPath(path){
+		console.log(path);
+		self.ctx.strokeStyle = path.color;
+		self.ctx.lineWidth = path.brush.size;
 		self.ctx.beginPath();
-		path.forEach(function(point, index){
+		path.points.forEach(function(point, index){
 			if(index == 0)
 				return;
-			self.ctx.moveTo(path[index-1][0],path[index-1][1]);
+			self.ctx.moveTo(path.points[index-1][0],path.points[index-1][1]);
 			self.ctx.lineTo(point[0], point[1]);
 			self.ctx.closePath();
 			self.ctx.stroke();
@@ -78,7 +91,8 @@ jsd.Canvas.prototype.refresh = function(){
 
 	this.paths.forEach(drawPath)
 
-	drawPath(this.currentPath);
+	if(this.currentPath.length>0)
+		drawPath({points:this.currentPath,color: this.context.colors.fg, brush: _.clone(this.context.brush)});
 
 	setTimeout( function(){
 		self.refresh();
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,41 @@
 "use strict";
 
 var context = {
-	params: {}
+	params: {
+		mode: "pencil"	
+	},
+	colors: {
+		fg: "#000000",
+		bg: "#ffffff"
+	},
+	brush: {
+		size: 1
+	},
+	dimentions: {
+		width: 640,
+		height: 480
+	}
 }
 
-var canvas = new jsd.Canvas("canvas.canvas");
+var canvas = new jsd.Canvas("canvas.canvas", context);
 
-canvas.init();
+$(document).ready(function(){
+	canvas.init();
+
+	$("#canvas-width").change(function(){
+		context.dimentions.width = $(this).val();
+	})
+
+	$("#canvas-height").change(function(){
+		context.dimentions.height = $(this).val();
+	})
+
+	$("#color-fg").change(function(){
+		context.colors.fg = $(this).val();
+	})
+
+	$("#brush-size").change(function(){
+		context.brush.size = $(this).val();
+	})
+
+})
